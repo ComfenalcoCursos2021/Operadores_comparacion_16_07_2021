c8a2409e6d0b9255c117eab6d069c361f77a96b5
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-07-16 21:01:51 -0500

    Operadores de Comparacion

---
+++ main.js
@@ -0,0 +1,28 @@
+
+//Pedir 2 datos por pantalla y almacenarlo en dos variables 
+//diferentes:
+//Variable 1(8)  Variable 2 (20)
+//Realizar todas las Operacion compracion vistas en la clase
+//Mostra en la consola todos los posibles resultados
+//y explicar con sus propias palabras porque se dio es resultado 'true , false'
+
+
+let a = 5, b = '5';
+
+//Operador Mayor que
+console.log(`El a = ${a} es mayor que el b = ${b} :`,a>b);
+//Operador Mayor o igual que
+console.log(`El a = ${a} es mayor o igual que el b = ${b} :`,a>=b);
+//Operador Menor que
+console.log(`El a = ${a} es menor que el b = ${b} :`,a<b);
+//Operador Menor o igual que
+console.log(`El a = ${a} es menor o igual que el b = ${b} :`,a<=b);
+
+// Operador Igual que
+console.log(`El a = ${a} es igual que el b = ${b} :`,a==b);
+//Operador Identico que
+console.log(`El a = '${a} ${typeof a}' es identico que el b = '${b} ${typeof b}' :`,a===b);
+//Operador Diferente que
+console.log(`El a = ${a} es diferente que el b = ${b} :`,a!=b);
+//Operador No Diferente que
+console.log(`El a = '${a} ${typeof a}' no diferente que b = '${b} ${typeof b}' :`,a!==b);
